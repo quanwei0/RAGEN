652b67a951f448888a8b262a3524b628f21fa086
Quan Wei <weiquan0128@gmail.com>
2025-06-13 18:46:57 +0000

    update 1.5b train

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ def run_ppo(config) -> None:
                 'TOKENIZERS_PARALLELISM': 'true',
                 'NCCL_DEBUG': 'WARN',
                 'VLLM_LOGGING_LEVEL': 'WARN',
-                "RAY_DEBUG": "legacy" # used here for simpler breakpoint()
+                # "RAY_DEBUG": "legacy" # used here for simpler breakpoint()
             }
         })
 
--- train_ppo.sh
+++ train_ppo.sh
@@ -9,8 +9,8 @@ USE_PPO="algorithm.adv_estimator=gae" # by default.
 USE_BASE="algorithm.kl_ctrl.kl_coef=0.001 actor_rollout_ref.actor.kl_loss_coef=0.001 actor_rollout_ref.actor.clip_ratio_high=0.2 actor_rollout_ref.rollout.rollout_filter_ratio=1"
 
 # token-level GAE
-MKL_SERVICE_FORCE_INTEL=1 python train.py --config-name webshop_3b_train system.CUDA_VISIBLE_DEVICES=\"2,3,6,7\" trainer.n_gpus_per_node=4 \
-    trainer.experiment_name=webshop-3b-ppo $USE_PPO $USE_BASE \
+MKL_SERVICE_FORCE_INTEL=1 python train.py --config-name webshop_1.5b_train system.CUDA_VISIBLE_DEVICES=\"0,1,2,3\" trainer.n_gpus_per_node=4 \
+    trainer.experiment_name=qw-webshop-1.5b-ppo $USE_PPO $USE_BASE \
     es_manager.train.env_groups=2 es_manager.train.group_size=16 es_manager.train.env_configs.n_groups=[2] \
     trainer.nnodes=1
 
